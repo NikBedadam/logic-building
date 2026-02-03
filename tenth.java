d438cc02b541bb64f484cd768db9d62fc1285996
Nikhitha <nikbedadam@gmail.com>
2026-02-03 10:17:03 +0530

    commit

---
+++ tenth.java
@@ -0,0 +1,29 @@
+public class tenth {
+    public static void main(String args[]){
+        int n=10;
+        for(int i=0;i<=n;i++){
+            for(int j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            for(int j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=2*i-1;k++){
+                System.out.print("*");
+            }
+            for(int j=n-i;j>=1;j--){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
